23e0a27aa6c61e1266cc0f523c2ab1047d856f5a
Zhavi221 <netalevy2059@gmail.com>
2020-03-25 22:36:17 +0200

    Initial Commit
    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApp_TimeZones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NorthAmericaTimeZone1 = new System.Windows.Forms.Label();
+            this.NorthAmericaTimeZone2 = new System.Windows.Forms.Label();
+            this.SouthAmericaTimeZone = new System.Windows.Forms.Label();
+            this.AfricaTimeZone1 = new System.Windows.Forms.Label();
+            this.AfricaTimeZone2 = new System.Windows.Forms.Label();
+            this.EuropeTimeZone1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EuropeTimeZone2 = new System.Windows.Forms.Label();
+            this.AsiaTimeZone1 = new System.Windows.Forms.Label();
+            this.AsiaTimeZone2 = new System.Windows.Forms.Label();
+            this.AsiaTimeZone3 = new System.Windows.Forms.Label();
+            this.AustraliaTimeZone = new System.Windows.Forms.Label();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.GreenLandTimeZone = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp_TimeZones.Properties.Resources.world_best_continents_map;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 452);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // NorthAmericaTimeZone1
+            // 
+            this.NorthAmericaTimeZone1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(134)))), ((int)(((byte)(139)))));
+            this.NorthAmericaTimeZone1.Location = new System.Drawing.Point(106, 86);
+            this.NorthAmericaTimeZone1.Name = "NorthAmericaTimeZone1";
+            this.NorthAmericaTimeZone1.Size = new System.Drawing.Size(56, 85);
+            this.NorthAmericaTimeZone1.TabIndex = 1;
+            this.NorthAmericaTimeZone1.Click += new System.EventHandler(this.NorthAmericaTimeZone_Click);
+            // 
+            // NorthAmericaTimeZone2
+            // 
+            this.NorthAmericaTimeZone2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(134)))), ((int)(((byte)(139)))));
+            this.NorthAmericaTimeZone2.Location = new System.Drawing.Point(135, 134);
+            this.NorthAmericaTimeZone2.Name = "NorthAmericaTimeZone2";
+            this.NorthAmericaTimeZone2.Size = new System.Drawing.Size(65, 37);
+            this.NorthAmericaTimeZone2.TabIndex = 2;
+            this.NorthAmericaTimeZone2.Click += new System.EventHandler(this.NorthAmericaTimeZone_Click);
+            // 
+            // SouthAmericaTimeZone
+            // 
+            this.SouthAmericaTimeZone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(112)))), ((int)(((byte)(40)))));
+            this.SouthAmericaTimeZone.Location = new System.Drawing.Point(224, 257);
+            this.SouthAmericaTimeZone.Name = "SouthAmericaTimeZone";
+            this.SouthAmericaTimeZone.Size = new System.Drawing.Size(41, 63);
+            this.SouthAmericaTimeZone.TabIndex = 3;
+            this.SouthAmericaTimeZone.Click += new System.EventHandler(this.SouthAmericaTimeZone_Click);
+            // 
+            // AfricaTimeZone1
+            // 
+            this.AfricaTimeZone1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(156)))), ((int)(((byte)(71)))));
+            this.AfricaTimeZone1.Location = new System.Drawing.Point(355, 188);
+            this.AfricaTimeZone1.Name = "AfricaTimeZone1";
+            this.AfricaTimeZone1.Size = new System.Drawing.Size(90, 43);
+            this.AfricaTimeZone1.TabIndex = 4;
+            this.AfricaTimeZone1.Click += new System.EventHandler(this.AfricaTimeZone_Click);
+            // 
+            // AfricaTimeZone2
+            // 
+            this.AfricaTimeZone2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(156)))), ((int)(((byte)(71)))));
+            this.AfricaTimeZone2.Location = new System.Drawing.Point(410, 202);
+            this.AfricaTimeZone2.Name = "AfricaTimeZone2";
+            this.AfricaTimeZone2.Size = new System.Drawing.Size(43, 94);
+            this.AfricaTimeZone2.TabIndex = 5;
+            this.AfricaTimeZone2.Click += new System.EventHandler(this.AfricaTimeZone_Click);
+            // 
+            // EuropeTimeZone1
+            // 
+            this.EuropeTimeZone1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(103)))), ((int)(((byte)(170)))));
+            this.EuropeTimeZone1.Location = new System.Drawing.Point(446, 100);
+            this.EuropeTimeZone1.Name = "EuropeTimeZone1";
+            this.EuropeTimeZone1.Size = new System.Drawing.Size(53, 27);
+            this.EuropeTimeZone1.TabIndex = 6;
+            this.EuropeTimeZone1.Click += new System.EventHandler(this.EuropeTimeZone_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(156)))), ((int)(((byte)(71)))));
+            this.label1.Location = new System.Drawing.Point(422, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 7;
+            // 
+            // EuropeTimeZone2
+            // 
+            this.EuropeTimeZone2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(103)))), ((int)(((byte)(170)))));
+            this.EuropeTimeZone2.Location = new System.Drawing.Point(422, 121);
+            this.EuropeTimeZone2.Name = "EuropeTimeZone2";
+            this.EuropeTimeZone2.Size = new System.Drawing.Size(53, 19);
+            this.EuropeTimeZone2.TabIndex = 7;
+            this.EuropeTimeZone2.Click += new System.EventHandler(this.EuropeTimeZone_Click);
+            // 
+            // AsiaTimeZone1
+            // 
+            this.AsiaTimeZone1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(54)))));
+            this.AsiaTimeZone1.Location = new System.Drawing.Point(554, 70);
+            this.AsiaTimeZone1.Name = "AsiaTimeZone1";
+            this.AsiaTimeZone1.Size = new System.Drawing.Size(81, 126);
+            this.AsiaTimeZone1.TabIndex = 8;
+            this.AsiaTimeZone1.Click += new System.EventHandler(this.AsiaTimeZone_Click);
+            // 
+            // AsiaTimeZone2
+            // 
+            this.AsiaTimeZone2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(54)))));
+            this.AsiaTimeZone2.Location = new System.Drawing.Point(520, 86);
+            this.AsiaTimeZone2.Name = "AsiaTimeZone2";
+            this.AsiaTimeZone2.Size = new System.Drawing.Size(153, 62);
+            this.AsiaTimeZone2.TabIndex = 9;
+            this.AsiaTimeZone2.Click += new System.EventHandler(this.AsiaTimeZone_Click);
+            // 
+            // AsiaTimeZone3
+            // 
+            this.AsiaTimeZone3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(54)))));
+            this.AsiaTimeZone3.Location = new System.Drawing.Point(503, 148);
+            this.AsiaTimeZone3.Name = "AsiaTimeZone3";
+            this.AsiaTimeZone3.Size = new System.Drawing.Size(98, 48);
+            this.AsiaTimeZone3.TabIndex = 10;
+            this.AsiaTimeZone3.Click += new System.EventHandler(this.AsiaTimeZone_Click);
+            // 
+            // AustraliaTimeZone
+            // 
+            this.AustraliaTimeZone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(61)))), ((int)(((byte)(150)))));
+            this.AustraliaTimeZone.Location = new System.Drawing.Point(642, 298);
+            this.AustraliaTimeZone.Name = "AustraliaTimeZone";
+            this.AustraliaTimeZone.Size = new System.Drawing.Size(58, 22);
+            this.AustraliaTimeZone.TabIndex = 11;
+            this.AustraliaTimeZone.Click += new System.EventHandler(this.AustraliaTimeZone_Click);
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(234)))), ((int)(((byte)(253)))));
+            this.MainLabel.Font = new System.Drawing.Font("Comic Sans MS", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLabel.Location = new System.Drawing.Point(254, 385);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(575, 67);
+            this.MainLabel.TabIndex = 12;
+            this.MainLabel.Text = "Click On Any Contintent!";
+            // 
+            // GreenLandTimeZone
+            // 
+            this.GreenLandTimeZone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(134)))), ((int)(((byte)(139)))));
+            this.GreenLandTimeZone.Location = new System.Drawing.Point(263, 20);
+            this.GreenLandTimeZone.Name = "GreenLandTimeZone";
+            this.GreenLandTimeZone.Size = new System.Drawing.Size(36, 59);
+            this.GreenLandTimeZone.TabIndex = 13;
+            this.GreenLandTimeZone.Click += new System.EventHandler(this.GreenLandTimeZone_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(234)))), ((int)(((byte)(253)))));
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(575, 67);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Click On Any Contintent!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GreenLandTimeZone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.AustraliaTimeZone);
+            this.Controls.Add(this.AsiaTimeZone3);
+            this.Controls.Add(this.AsiaTimeZone2);
+            this.Controls.Add(this.AsiaTimeZone1);
+            this.Controls.Add(this.EuropeTimeZone2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EuropeTimeZone1);
+            this.Controls.Add(this.AfricaTimeZone2);
+            this.Controls.Add(this.AfricaTimeZone1);
+            this.Controls.Add(this.SouthAmericaTimeZone);
+            this.Controls.Add(this.NorthAmericaTimeZone2);
+            this.Controls.Add(this.NorthAmericaTimeZone1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label NorthAmericaTimeZone1;
+        private System.Windows.Forms.Label NorthAmericaTimeZone2;
+        private System.Windows.Forms.Label SouthAmericaTimeZone;
+        private System.Windows.Forms.Label AfricaTimeZone1;
+        private System.Windows.Forms.Label AfricaTimeZone2;
+        private System.Windows.Forms.Label EuropeTimeZone1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label EuropeTimeZone2;
+        private System.Windows.Forms.Label AsiaTimeZone1;
+        private System.Windows.Forms.Label AsiaTimeZone2;
+        private System.Windows.Forms.Label AsiaTimeZone3;
+        private System.Windows.Forms.Label AustraliaTimeZone;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Label GreenLandTimeZone;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_TimeZones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void NorthAmericaTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime NorthAmerica = DateTime.Now;
+            NorthAmerica = NorthAmerica.AddHours(-6);
+            MessageBox.Show(NorthAmerica.ToLongTimeString(), "Time In North America");
+        }
+
+        private void SouthAmericaTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime SouthAmerica = DateTime.Now;
+            SouthAmerica = SouthAmerica.AddHours(-3);
+            MessageBox.Show(SouthAmerica.ToLongTimeString(), "Time In South America");
+        }
+
+        private void AfricaTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime Africa = DateTime.Now;
+            Africa = Africa.AddHours(-2);
+            MessageBox.Show(Africa.ToLongTimeString(), "Time In Africa");
+        }
+
+        private void EuropeTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime Europe = DateTime.Now;
+            Europe = Europe.AddHours(1);
+            MessageBox.Show(Europe.ToLongTimeString(), "Time In Europe");
+        }
+
+        private void AsiaTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime Asia = DateTime.Now;
+            Asia = Asia.AddHours(6);
+            MessageBox.Show(Asia.ToLongTimeString(), "Time In Asia");
+        }
+
+        private void AustraliaTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime Australia = DateTime.Now;
+            Australia = Australia.AddHours(9);
+            MessageBox.Show(Australia.ToLongTimeString(), "Time In Australia");
+        }
+
+        private void GreenLandTimeZone_Click(object sender, EventArgs e)
+        {
+            DateTime GreenLand = DateTime.Now;
+            GreenLand = GreenLand.AddHours(-2);
+            MessageBox.Show(GreenLand.ToLongTimeString(), "Time In GreenLand");
+        }
+    }
+}
